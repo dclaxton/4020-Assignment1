3bb6f177a441fee8e7cd6bd89d1eeee1e79725d8
danielsworld <didacticdaniel@gmail.com>
2020-02-12 12:55:58 -0600

    Added isWinner algorithm and image files that were missing in last commit

--- app/src/main/java/csci/apsu/tictactoe/PlayNumericalActivity.java
+++ app/src/main/java/csci/apsu/tictactoe/PlayNumericalActivity.java
@@ -13,4 +13,49 @@ public class PlayNumericalActivity extends AppCompatActivity implements View.OnC
     public void onClick(View view) {
 
     }
+
+    // Algorithm that scans for a sum of 15 anywhere on the board
+    public boolean isWinner() {
+        // Check horizontal
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length - 2; col++) {
+                if (board[row][col] + board[row][col + 1] +
+                        board[row][col + 2] == 15) {
+                    return true;
+                }
+            }
+        }
+
+        // Check vertical
+        for (int row = 0; row < board.length - 2; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                if (board[row][col] + board[row + 1][col] +
+                        board[row + 2][col] == 15) {
+                    return true;
+                }
+            }
+        }
+
+        // Check diagonal
+        for (int row = 0; row < board.length - 2; row++) {
+            for (int col = 0; col < board[row].length - 2; col++) {
+                if (board[row][col] + board[row + 1][col + 1] +
+                        board[row + 2][col + 2] == 15) {
+                    return true;
+                }
+            }
+        }
+
+        // Check reverse-diagonal
+        for (int row = 0; row < board.length - 2; row++) {
+            for (int col = 4; col < board[row].length; col++) {
+                if (board[row][col] + board[row + 1][col - 1] +
+                        board[row + 2][col - 2] == 15) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 }
