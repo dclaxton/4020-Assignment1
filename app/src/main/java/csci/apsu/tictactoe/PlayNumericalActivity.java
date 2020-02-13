56462700763f6504fcb041fb50bf68c840a8375a
danielsworld <didacticdaniel@gmail.com>
2020-02-13 16:39:00 -0600

    Added function to set the on click listeners of Image Views in the game board and initialize the game board array

--- app/src/main/java/csci/apsu/tictactoe/PlayNumericalActivity.java
+++ app/src/main/java/csci/apsu/tictactoe/PlayNumericalActivity.java
@@ -2,6 +2,11 @@ package csci.apsu.tictactoe;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PlayNumericalActivity extends AppCompatActivity implements View.OnClickListener {
@@ -9,6 +14,8 @@ public class PlayNumericalActivity extends AppCompatActivity implements View.OnC
     public static final int ROWS = 6, COLS = 6;
     public static int[][] board = new int[ROWS][COLS];
     public static int moves = 0;
+
+    public TextView turn_textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,27 @@ public class PlayNumericalActivity extends AppCompatActivity implements View.OnC
     @Override
     public void onClick(View view) {
 
+    }
+
+    private void initializeBoard() {
+        View child;
+        ViewGroup layout = findViewById(R.id.board_linearLayout);
+        turn_textView = findViewById(R.id.player_turn_textView);
+        moves = 0;
+
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            child = layout.getChildAt(i);
+            if (child instanceof LinearLayout) {
+                ViewGroup rowLayout = findViewById(child.getId());
+                for (int j = 0; j < rowLayout.getChildCount(); j++) {
+                    child = rowLayout.getChildAt(j);
+                    if (child instanceof ImageView) {
+                        child.setOnClickListener(this);
+                        board[i][j] = child.getId();
+                    }
+                }
+            }
+        }
     }
 
     // Algorithm that scans for a sum of 15 anywhere on the board
